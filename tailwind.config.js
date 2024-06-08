6924df11afb3759997b2bcef11318e97619fb742
aliansyah <138671034+bidking@users.noreply.github.com>
2024-06-08 12:17:37 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+    purge: [],
+    darkMode: false,
+    theme: {
+      extend: {
+        colors: {
+          'custom-green': '#00D154',
+          'custom-dark-green': '#00923a',
+          'custom-light-green': '#00ff66',
+        },
+        spacing: {
+          '200p': '200%',
+          '50p': '50%',
+          '40p': '40%',
+          '49p': '49%',
+          '300px': '300px',
+          '27px': '27px',
+          '50px': '50px',
+        },
+        borderRadius: {
+          'none': '0%',
+          '30p': '30%',
+          'full': '50%',
+        },
+        transform: {
+          'rotate-25': 'rotate(25deg)',
+          'rotate-28': 'rotate(28deg)',
+        },
+        zIndex: {
+          '-10': '-10',
+          '-9': '-9',
+          '-20': '-20',
+        },
+        height: {
+          '1000px': '1000px',
+          '60vh': '60vh',
+        },
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
